65ac8f8c933767f3879e06fd3d8215f92f15351c
James Aules <james_aules@icloud.com>
2022-09-21 15:52:13 -0400

    Making our Code

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your Code Here
+// variables
+let UserName = window.prompt("Enter Your name")
+let Userscore = 0
+let Playagain = true
+ // Making a funtion that has inside de for loop this is for the while repetition
+function playGame (){
+    //For loop so each question is prompt
+for(let i = 0; i < questions.length; i++) {
+    let question = questions[i]
+    let UserAnswer = window.prompt(question.text)
+ // if so we can add the score
+if (UserAnswer === question.correctAnswer) {
+     Userscore = Userscore + 10
+    }
+}
+// window ALERT so you can see the score
+window.alert ("Your score is "+Userscore)
+}
+// While loop  so you can restart the game 
+while (Playagain ===true) {
+    playGame()
+    let UserChoice = window.prompt("Wanna do it again Y o N")
+if (UserChoice === 'Y') {
+    Playagain = true
+} else {
+    Playagain = false
+    window.alert("GameOver")
+    // prompt game over & set false so the game stops
+}
+}
